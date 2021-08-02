76120336dc1acdcb1a7980d224a800b753a08aee
dipakskywave <dipak.skywaveinfo@gmail.com>
2021-08-02 15:14:57 +0530

    Commit 02_08_2021_1.

---
+++ app/src/main/java/com/skywave/callpeon/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.skywave.callpeon
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    var btnSend: Button? = null
+    private val FCM_API = "https://fcm.googleapis.com/fcm/send"
+    private val serverKey =
+        "key=" + "AAAAZPWrNgw:APA91bExsMk3HoYxA19IuUIW-qIsNiwnVUTsScvJPVNaxezSnlF2Q-O2hlSVneHer9CmqEfVESJTefx0Ns9RuRul9Q3isaAfo0Whx0M5SfboKkRXzVMeerMbHGPc5Ayo6RicN6IUtbL7"
+    private val contentType = "application/json"
+    val TAG = "NOTIFICATION TAG"
+    private var requestQueue: RequestQueue? = null
+
+    var NOTIFICATION_TITLE: String? = null
+    var NOTIFICATION_MESSAGE: String? = null
+    var TOPIC: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestQueue = getRequestQueue()
+        btnSend = findViewById(R.id.btnSend)
+//        Company LoggedIn
+//        btnSend?.visibility = View.GONE
+        btnSend?.setOnClickListener {
+            sendNotification()
+        }
+    }
+
+    private fun sendNotification() {
+        TOPIC = "/topics/userABC" //topic must match with what the receiver subscribed to
+
+        NOTIFICATION_TITLE = "Please"
+        NOTIFICATION_MESSAGE = "Come In"
+
+        val notification = JSONObject()
+        val notifcationBody = JSONObject()
+        try {
+            notifcationBody.put("title", NOTIFICATION_TITLE)
+            notifcationBody.put("message", NOTIFICATION_MESSAGE)
+            notification.put("to", TOPIC)
+            notification.put("data", notifcationBody)
+        } catch (e: JSONException) {
+            Log.e(TAG, "onCreate: " + e.message)
+        }
+        sendNotification1(notification)
+    }
+
+    private fun sendNotification1(notification: JSONObject) {
+        val jsonObjectRequest: JsonObjectRequest = object : JsonObjectRequest(FCM_API, notification,
+            Response.Listener<JSONObject?> {
+                Log.i(TAG, "onResponse: $it")
+            },
+            Response.ErrorListener {
+                Toast.makeText(this@MainActivity, "Request error", Toast.LENGTH_LONG).show()
+                Log.i(TAG, "onErrorResponse: Didn't work")
+            }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = serverKey
+                params["Content-Type"] = contentType
+                return params
+            }
+        }
+        getRequestQueue()!!.add(jsonObjectRequest)
+    }
+
+    private fun getRequestQueue(): RequestQueue? {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(applicationContext)
+        }
+        return requestQueue
+    }
+}
---
+++ app/src/main/java/com/skywave/callpeon/MyFirebaseMessagingService.kt
@@ -0,0 +1,105 @@
+package com.skywave.callpeon
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import java.util.*
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val ADMIN_CHANNEL_ID = "admin_channel"
+    private val TAG = "MyFirebaseMessagingService"
+    private val SUBSCRIBE_TO = "userABC"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationID: Int = Random().nextInt(3000)
+        Log.i(TAG, "remoteMessage============>>>>>>: ${remoteMessage.data}")
+
+        /*
+        Apps targeting SDK 26 or above (Android O) must implement notification channels and add its notifications
+        to at least one of them. Therefore, confirm if version is Oreo or higher, then setup notification channel
+      */if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setupChannels(notificationManager)
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+        val largeIcon = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.app_icon
+        )
+
+//        val notificationSoundUri: Uri =
+//            Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/" + R.raw.sound)
+//            Settings.System.DEFAULT_RINGTONE_URI
+        val notificationBuilder: NotificationCompat.Builder =
+            NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.app_icon)
+                .setLargeIcon(largeIcon)
+                .setContentTitle(remoteMessage.data["title"])
+                .setContentText(remoteMessage.data["message"])
+                .setAutoCancel(true)
+//                .setSound(notificationSoundUri, AudioManager.STREAM_ALARM)
+                .setContentIntent(pendingIntent)
+        //Set notification color to match your app color template
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            notificationBuilder.color = resources.getColor(R.color.black)
+        }
+        notificationManager.notify(notificationID, notificationBuilder.build())
+        if (remoteMessage.data["title"]!!.isNotEmpty()) {
+            var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.sound)
+            if (!mediaPlayer?.isPlaying!!) {
+                mediaPlayer?.start()
+            } else {
+//                mediaPlayer.stop()
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private fun setupChannels(notificationManager: NotificationManager?) {
+        val adminChannelName: CharSequence = "New notification"
+        val adminChannelDescription = "Device to device notification"
+//        val sound =
+//            Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/raw/sound.mp3")
+//        val attributes = AudioAttributes.Builder()
+//            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+//            .build()
+        val adminChannel = NotificationChannel(
+            ADMIN_CHANNEL_ID,
+            adminChannelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        adminChannel.description = adminChannelDescription
+        adminChannel.enableLights(true)
+        adminChannel.lightColor = Color.RED
+        adminChannel.enableVibration(true)
+//        adminChannel.setSound(sound, attributes)
+        notificationManager?.createNotificationChannel(adminChannel)
+    }
+
+    override fun onNewToken(p0: String) {
+        // Once the token is generated, subscribe to topic with the userId
+        FirebaseMessaging.getInstance().subscribeToTopic(SUBSCRIBE_TO)
+        Log.i(TAG, "onTokenRefresh completed with token============>>>>>>   : $p0")
+    }
+
+}
